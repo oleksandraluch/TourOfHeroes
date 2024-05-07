5116c3dc806395b5f839b57ca67167d214117927
oleksandraluch <oluchakova@myseneca.ca>
2024-05-07 14:18:35 -0400

    comment out mock data

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -16,7 +16,9 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 17, name: 'Dynama' },
       { id: 18, name: 'Dr. IQ' },
       { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 20, name: 'Tornado' },
+      { id: 21, name: 'OneMore' },
+      { id: 22, name: 'TwoMore' }
     ];
     return {heroes};
   }
